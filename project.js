9566cd1e1526ec3b59d635c5fdd43debccd3e20f
Christian Kretschmer <Cjkretschmer@gmail.com>
2016-04-10 18:23:36 -0400

    project 1

---
+++ project.js
@@ -0,0 +1,52 @@
+"use strict";
+
+(function(){
+$(document).ready(function(){
+
+
+    var myArray = [
+[null, null, null],
+[null, null, null],
+[null, null, null],
+]
+
+});
+
+var topleft = 0;
+$('#tl').click(function(){    // I know that I want the click function
+  console.log('click')        // but I'm not sure how it's supposed to apply.
+  if (topleft === 0){
+    $(this).css("");    //I don't know what goes in the quotes
+    topleft++;
+  } else if () {
+    $(this).css("");    //I don't know what goes in the quotes
+    topleft = 0;
+  }
+});
+
+var topmiddle = 0;
+$('#tm').click(function(){
+  console.log('click')
+  if (topmiddle === 0){
+    $(this).css("");
+    topmiddle++;
+  }  else if () {
+
+
+  }
+  }
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+};
